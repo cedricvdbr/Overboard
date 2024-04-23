0620042cd787a8fcdec00678874a4c479ad81de1
1kuraudo <thomas.verbocht@gmail.com>
2024-04-23 12:27:39 +0200

    grid + tile-based movement

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int _floorWidth = 10;
+    [SerializeField]
+    private int _floorHeight = 10;
+    [SerializeField]
+    private GameObject _tilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateFloor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateFloor()
+    {
+        Vector3 floorPosition = new Vector3(-_floorWidth / 2, 0, -_floorHeight / 2);
+
+        for (int x = 0; x < _floorWidth; x++)
+        {
+            for (int z = 0; z < _floorHeight; z++)
+            {
+                Vector3 tilePosition = new Vector3(floorPosition.x + x, 0, floorPosition.z + z);
+                Instantiate(_tilePrefab, tilePosition, _tilePrefab.transform.rotation);
+            }
+        }
+    }
+}
